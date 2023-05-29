3f866b8dd528ee58a059b4b718e6fc1edf8b3735
Anamika Sharma <112106640+Anamika-29@users.noreply.github.com>
2023-05-29 22:16:01 +0530

    Initial commit

--- serverLessonHttp.js
+++ serverLessonHttp.js
@@ -6,20 +6,28 @@ app.use(cors());
 app.use(express.json());
 
 app.post('/api/endpoint', async (req, res) => {
-  const { method, fetchURL, data,headerValue1 } = req.body;
+  const { method,
+    fetchURL,
+    data,
+    headerKey1=headerKey1.toLowerCase(),
+    headerValue1,
+    headerKey2=headerKey2.toLowerCase(),
+    headerValue2,
+    headerKey3=headerKey3.toLowerCase(),
+    headerValue3} = req.body;
 
   console.log(req.body);
 
   try {
     let axiosResponse;
     if (method === 'GET') {
-      axiosResponse = await axios.get(fetchURL,{headers: {authorization : headerValue1}});
+      axiosResponse = await axios.get(fetchURL,{headers: {headerKey1: headerValue1,headerKey2:headerValue2,headerKey3:headerValue3}});
     } else if (method === 'POST') {
-      axiosResponse = await axios.post(fetchURL, JSON.parse(data),{headers: {authorization : headerValue1}});
+      axiosResponse = await axios.post(fetchURL, JSON.parse(data),{headers: {headerKey1: headerValue1,headerKey2:headerValue2,headerKey3:headerValue3}});
     } else if (method === 'PUT') {
-      axiosResponse = await axios.put(fetchURL, JSON.parse(data),{headers: {authorization : headerValue1}});
+      axiosResponse = await axios.put(fetchURL, JSON.parse(data),{headers: {headerKey1: headerValue1,headerKey2:headerValue2,headerKey3:headerValue3}});
     } else if (method === 'DELETE') {
-      axiosResponse = await axios.delete(fetchURL,{headers: {authorization : headerValue1}});
+      axiosResponse = await axios.delete(fetchURL,{headers: {headerKey1: headerValue1,headerKey2:headerValue2,headerKey3:headerValue3}});
     }
     else {
       throw new Error('Invalid method');
